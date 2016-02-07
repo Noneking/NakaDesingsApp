78eb2541784b68c71b5bf098b223265cefaf4d99
Noneking <aanoriega.mobile@gmail.com>
2016-02-07 13:06:20 +0100

    Styles repaired

--- app/src/main/java/com/example/nonek/nakadesingsapp/MainActivity.java
+++ app/src/main/java/com/example/nonek/nakadesingsapp/MainActivity.java
@@ -2,28 +2,104 @@ package com.example.nonek.nakadesingsapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Components
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    //Variables
+    private String drawerTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            getMenuInflater().inflate(R.menu.navigation_menu, menu);
+//            return true;
+//        }
+
+        //Change color of Status Bar for Menu's Bar color
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.primaryColor));
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void initComponents(Bundle savedInstanceState){
+
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+
+        initListeners(savedInstanceState);
+    }
+
+    public void initListeners(Bundle savedInstanceState){
+
+
+
+        initOperations(savedInstanceState);
+    }
+
+    public void initOperations(Bundle savedInstanceState){
+
+        if (navigationView != null) {
+            // Añadir carácteristicas
+        }
+
+        drawerTitle = getResources().getString(R.string.home_item);
+        if (savedInstanceState == null) {
+            // Seleccionar item
+        }
+
+    }
+
+    private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 }
