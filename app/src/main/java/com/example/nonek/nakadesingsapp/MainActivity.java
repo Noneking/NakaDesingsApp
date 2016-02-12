474be5d5cb3807f9c78b1388ae248c47276549a2
Noneking <aanoriega.mobile@gmail.com>
2016-02-12 19:18:04 +0100

    Little changes

--- app/src/main/java/com/example/nonek/nakadesingsapp/MainActivity.java
+++ app/src/main/java/com/example/nonek/nakadesingsapp/MainActivity.java
@@ -10,10 +10,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -24,9 +22,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView navigationView;
 
     ProductActivity productActivity;
-    ProductFrament productFragment;
-
-    GestureListener gestureListener;
 
     //Variables
     private String drawerTitle;
@@ -87,11 +82,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     public void initOperations(Bundle savedInstanceState){
 
+        drawerTitle = getResources().getString(R.string.app_title);
+
         if (navigationView != null) {
             // Añadir carácteristicas
         }
 
-        drawerTitle = getResources().getString(R.string.category_item);
         if (savedInstanceState == null) {
             // Seleccionar item
         }
@@ -124,18 +120,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
                 break;
             case "Product":
-//                fragment = ProductActivity.newInstance(title);
 
-//                Intent intent=new Intent(getApplicationContext(), ProductActivity.class);
-//                fragment=startActivityFromFragment(R.id.main_content, intent, RESULT_OK);
-
-                productFragment=new ProductFrament();
+                productActivity=new ProductActivity();
 
                 FragmentManager fragmentManager=getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.main_content, productFragment).commit();
-
-//                productActivity=new ProductActivity(productFragment.getCurrentView());
+                fragmentTransaction.replace(R.id.main_content, productActivity).commit();
 
                 break;
             case "Employee":
@@ -149,105 +139,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
 
-//        if(fragment!=null){
-//            FragmentManager fragmentManager = getSupportFragmentManager();
-//            fragmentManager
-//                    .beginTransaction()
-//                    .replace(R.id.main_content, fragment)
-//                    .commit();
-
-//            android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
-//            fragmentTransaction.replace(R.id.product_mainLayout, Fragment.instantiate(getApplicationContext(), ProductActivity.class.getName()));
-//            fragmentTransaction.commit();
-//        }
-
         drawerLayout.closeDrawers();
         setTitle(title);
     }
-
-//    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
-//
-//        private static final int SWIPE_THRESHOLD = 100;
-//        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
-//
-//        @Override
-//        public boolean onDown(MotionEvent e) {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//            boolean result = false;
-//            try {
-//                float diffY = e2.getY() - e1.getY();
-//                float diffX = e2.getX() - e1.getX();
-//                if (Math.abs(diffX) > Math.abs(diffY)) {
-//                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-//                        if (diffX > 0) {
-//                            onSwipeRight();
-//                        } else {
-//                            onSwipeLeft();
-//                        }
-//                    }
-//                    result = true;
-//                }
-//                else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-//                    if (diffY > 0) {
-//                        onSwipeBottom();
-//                    } else {
-//                        onSwipeTop();
-//                    }
-//                }
-//                result = true;
-//
-//            } catch (Exception exception) {
-//                exception.printStackTrace();
-//            }
-//            return result;
-//        }
-//    }
-//
-//    public void onSwipeTop() {
-//        //Toast.makeText(this, "top", Toast.LENGTH_SHORT).show();
-//        animationBottom();
-//    }
-//    public void onSwipeRight() {
-//        //Toast.makeText(this, "right", Toast.LENGTH_SHORT).show();
-//        animationLeft();
-//    }
-//    public void onSwipeLeft() {
-//        //Toast.makeText(this, "left", Toast.LENGTH_SHORT).show();
-//        animationRight();
-//    }
-//    public void onSwipeBottom() {
-//        //Toast.makeText(this, "bottom", Toast.LENGTH_SHORT).show();
-//        animationTop();
-//    }
-//
-//    public void animationRight(){
-////        Toast.makeText(this,"animationRight",Toast.LENGTH_SHORT).show();
-////        if(tabhost.getCurrentTab()>1){
-////            tabhost.setCurrentTab(0);
-////        }else{
-////            tabhost.setCurrentTab(tabhost.getCurrentTab()+1);
-////        }
-//    }
-//
-//    public void animationLeft(){
-////        Toast.makeText(this,"animationLeft",Toast.LENGTH_SHORT).show();
-////        if(tabhost.getCurrentTab()==0){
-////            tabhost.setCurrentTab(1);
-////        }else{
-////            tabhost.setCurrentTab(tabhost.getCurrentTab() - 1);
-////        }
-//    }
-//
-//    public void animationTop(){
-//
-//    }
-//
-//    public void animationBottom(){
-//
-//    }
 
 }
--- app/src/main/java/com/example/nonek/nakadesingsapp/ProductActivity.java
+++ app/src/main/java/com/example/nonek/nakadesingsapp/ProductActivity.java
@@ -1,31 +1,28 @@
 package com.example.nonek.nakadesingsapp;
 
-import android.app.Activity;
 import android.content.res.Resources;
-import android.gesture.Gesture;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.SyncStateContract;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TextView;
 
-public class ProductActivity extends Activity implements TabHost.OnTabChangeListener {
+import static android.view.GestureDetector.*;
+
+/**
+ * Created by nonek on 11/02/2016.
+ */
+public class ProductActivity extends Fragment implements TabHost.OnTabChangeListener, TabHost.OnTouchListener {
 
     Resources res;
 
@@ -35,67 +32,48 @@ public class ProductActivity extends Activity implements TabHost.OnTabChangeList
     private TabHost.TabSpec spec;
     private LinearLayout tab_product_view, tab_product_insert;
 
-    public static GestureDetector gestureDetector;
+//    public static GestureDetector gestureDetector;
 
     private int currentTab;
 
-//    public static ProductActivity newInstance(String sectionTitle) {
-//
-//        ProductActivity fragment = new ProductActivity();
-//        return fragment;
-//    }
-//
-    public ProductActivity(View view){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
-        this.view=view;
+        this.view=inflater.inflate(R.layout.activity_product, container, false);
+        initFragmentUtilities();
+
+        return this.view;
+    }
+
+    public View getCurrentView(){
+        return this.view;
+    }
+
+    private void initFragmentUtilities() {
+
         initComponents();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
-
-//        initComponents();
-    }
-//
-//    public ProductActivity() {
-//    }
-
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-//
-//        this.view = inflater.inflate(R.layout.activity_product, container, false);
-//
-//        initFragmentUtilities();
-//
-//        return this.view;
-//    }
-
-//    public View getCurrentView(){
-//        return this.view;
-//    }
-
-//    private void initFragmentUtilities() {
-//
-//        initComponents();
-//    }
-
     private void initComponents() {
 
-        tabhost= (TabHost) this.findViewById(R.id.product_tabHost);
-        tab_product_view= (LinearLayout) this.findViewById(R.id.product_view_linearLayout);
-        tab_product_insert= (LinearLayout) this.findViewById(R.id.product_insert_linearLayout);
+        Log.i("tab", "PASA POR INITCOMPONENTS");
+
+        tabhost= (TabHost) this.view.findViewById(R.id.product_tabHost);
+        tab_product_view= (LinearLayout) this.view.findViewById(R.id.product_view_linearLayout);
+        tab_product_insert= (LinearLayout) this.view.findViewById(R.id.product_insert_linearLayout);
 
         initListeners();
     }
 
     private void initListeners() {
 
+        tabhost.setOnTouchListener(this);
 
         initOperations();
     }
 
     private void initOperations() {
+
+//        gestureDetector = new GestureDetector(this, new GestureListener());
 
         res=getResources();
         tabhost.setup();
@@ -111,6 +89,12 @@ public class ProductActivity extends Activity implements TabHost.OnTabChangeList
         spec.setContent(R.id.product_insert_linearLayout);
         spec.setIndicator("Insert", res.getDrawable(R.drawable.add1_128x128));
         tabhost.addTab(spec);
+
+        for(int i=0;i<tabhost.getTabWidget().getChildCount();i++)
+        {
+            TextView widgetTextView = (TextView) tabhost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            widgetTextView.setTextColor(Color.parseColor("#FFFFFF"));
+        }
 
         tabhost.setCurrentTab(0);
         currentTab=0;
@@ -130,10 +114,9 @@ public class ProductActivity extends Activity implements TabHost.OnTabChangeList
 
                 break;
         }
-
     }
 
-    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+    final GestureDetector gestureDetector=new GestureDetector(getActivity(), new GestureDetector.SimpleOnGestureListener(){
 
         private static final int SWIPE_THRESHOLD = 100;
         private static final int SWIPE_VELOCITY_THRESHOLD = 100;
@@ -145,25 +128,39 @@ public class ProductActivity extends Activity implements TabHost.OnTabChangeList
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
             boolean result = false;
+
             try {
                 float diffY = e2.getY() - e1.getY();
                 float diffX = e2.getX() - e1.getX();
                 if (Math.abs(diffX) > Math.abs(diffY)) {
                     if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
                         if (diffX > 0) {
-                            onSwipeRight();
+                            //Left Animation
+                            if(tabhost.getCurrentTab()==0){
+                                tabhost.setCurrentTab(1);
+                            }else{
+                                tabhost.setCurrentTab(tabhost.getCurrentTab()-1);
+                            }
                         } else {
-                            onSwipeLeft();
+                            //Right Animation
+                            if(tabhost.getCurrentTab()>1){
+                                tabhost.setCurrentTab(0);
+                            }else{
+                                tabhost.setCurrentTab(tabhost.getCurrentTab()+1);
+                            }
                         }
                     }
                     result = true;
                 }
                 else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
                     if (diffY > 0) {
-                        onSwipeBottom();
+                        //Top Animation
+
                     } else {
-                        onSwipeTop();
+                        //Bottom Animation
+
                     }
                 }
                 result = true;
@@ -173,51 +170,11 @@ public class ProductActivity extends Activity implements TabHost.OnTabChangeList
             }
             return result;
         }
-    }
+    });
 
-    public void onSwipeTop() {
-        //Toast.makeText(this, "top", Toast.LENGTH_SHORT).show();
-        animationBottom();
-    }
-    public void onSwipeRight() {
-        //Toast.makeText(this, "right", Toast.LENGTH_SHORT).show();
-        animationLeft();
-    }
-    public void onSwipeLeft() {
-        //Toast.makeText(this, "left", Toast.LENGTH_SHORT).show();
-        animationRight();
-    }
-    public void onSwipeBottom() {
-        //Toast.makeText(this, "bottom", Toast.LENGTH_SHORT).show();
-        animationTop();
-    }
-
-    public void animationRight(){
-//        Toast.makeText(this,"animationRight",Toast.LENGTH_SHORT).show();
-        Log.i("tab","Pasa por right");
-        if(tabhost.getCurrentTab()>0){
-            Log.i("tab","Entra dentro del primer if");
-            tabhost.setCurrentTab(0);
-        }else{
-            tabhost.setCurrentTab(tabhost.getCurrentTab()+1);
-        }
-    }
-
-    public void animationLeft(){
-//        Toast.makeText(this,"animationLeft",Toast.LENGTH_SHORT).show();
-        if(tabhost.getCurrentTab()==0){
-            tabhost.setCurrentTab(1);
-        }else{
-            tabhost.setCurrentTab(tabhost.getCurrentTab()-1);
-        }
-    }
-
-    public void animationTop(){
-
-    }
-
-    public void animationBottom(){
-
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
     }
 
 }
